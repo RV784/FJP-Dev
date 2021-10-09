e953c99a3d69eb8eda7f0d4a25a95b1b1c9a37c0
Rajat Verma <rajat.verma78402@gmail.com>
2021-10-09 19:44:54 +0530

    Support for making excel files from the data

--- Activities/Activity_1/1_CricinfoExtractor.js
+++ Activities/Activity_1/1_CricinfoExtractor.js
@@ -1,12 +1,14 @@
 
 
-// node 1_CricinfoExtracter.js --excel=Worldcup.csv --dataFolder=data --source=https://www.espncricinfo.com/series/icc-cricket-world-cup-2019-1144415/match-results 
+// node 1_CricinfoExtractor.js --excel=Worldcup.csv --dataFolder=data --source=https://www.espncricinfo.com/series/icc-cricket-world-cup-2019-1144415/match-results 
 
 let minimist = require("minimist");
 let axios = require("axios");
 let jsdom = require("jsdom");
-let excel4node = require("excel4node");
+let excel = require("excel4node");
 let pdf = require("pdf-lib");
+let fs = require("fs");
+//const { exec } = require("child_process");
 
 let args = minimist(process.argv);
 
@@ -50,11 +52,130 @@ responseKaPromise.then(function(response){
         matches.push(match);
         // console.log(i);
     }
-    console.log(matches);
-    console.log(matches);
+
+    // console.log(matches);
+    let teams = [];
+
+        for(let i = 0 ; i < matches.length ; i++){
+            populateTeams(teams, matches[i]); //puts individual team in teams array if missing
+        }
+
+        for(let i = 0 ; i < matches.length ; i++){
+            putMatchesInAppropriateTeam(teams, matches[i]); //puts individual team in teams array if missing
+        }
+
+    //console.log(JSON.stringify(teams));
+    let teamsJSON = JSON.stringify(teams);
+    fs.writeFileSync("teams.json", teamsJSON, "utf-8");   
+
+    //Here I'll create excel file 
+    createExcelfile(teams);
+    
+
 }).catch(function(err){
     console.log(err);
 })
+
+function populateTeams(teams, match){ // from matches array to teams array
+    // let t1idx = teams.findIndex(function(team){   //A loop runs on teams array, and returns each team to function
+    //     if(team.name == match.t1){   //we check team's name is similar to t1 team of match array's element or not
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // });
+    let t1idx = -1;
+    for(let i = 0 ; i < teams.length ; i++){
+        if(teams[i].name == match.t1){
+            t1idx = i;
+            break;
+        }
+    }
+
+    if(t1idx == -1){
+        let team = {
+            name: match.t1,
+            matches:[]
+        };
+        teams.push(team);
+    }
+
+    
+    let t2idx = -1;
+    for(let i = 0 ; i < teams.length ; i++){
+        if(teams[i].name == match.t2){
+            t2idx = i;
+            break;
+        }
+    }
+    if(t2idx == -1){
+        let team = {
+            name: match.t2,
+            matches:[]
+        };
+        teams.push(team);
+    }
+
+    
+}
+
+function putMatchesInAppropriateTeam(teams, match){
+    let t1idx = -1;
+    for(let i = 0 ; i < teams.length ; i++){
+        if(teams[i].name == match.t1){
+            t1idx = i;
+            break;
+        }
+    }
+
+    let team1 = teams[t1idx];
+    //console.log(team1);
+    team1.matches.push({
+        vs: match.t2,
+        selfScore: match.t1s,
+        oppScore: match.t2s,
+        result: match.result
+    });
+     
+
+    let t2idx = -1;
+    for(let i = 0 ; i < teams.length ; i++){
+        if(teams[i].name == match.t2){
+            t2idx = i;
+            break;
+        }
+    }
+
+    let team2 = teams[t2idx];
+    team2.matches.push({
+        vs: match.t1,
+        selfScore: match.t2s,
+        oppScore: match.t1s,
+        result: match.result
+    });
+
+}
+
+function createExcelfile(teams){
+    let wb = new excel.Workbook();
+
+    for(let i = 0 ; i < teams.length ; i++){
+        let sheet = wb.addWorksheet(teams[i].name);
+        
+        sheet.cell(1,1).string("VS");
+        sheet.cell(1,2).string("Self score");
+        sheet.cell(1,3).string("Opp Score");
+        sheet.cell(1,4).string("Results");
+
+        for(let j = 0 ; j < teams[i].matches.length ; j++){
+            sheet.cell(j+3,1).string(teams[i].matches[j].vs);
+            sheet.cell(j+3,2).string(teams[i].matches[j].selfScore);
+            sheet.cell(j+3,3).string(teams[i].matches[j].oppScore);
+            sheet.cell(j+3,4).string(teams[i].matches[j].result);
+        } 
+    }
+    wb.write(args.excel);
+}
 
 
 //Rajat Verma
